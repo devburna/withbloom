36e13362f4debb0cf7e3ede5709940fa646386b8
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-10-07 21:29:24 +0100

    chore:

--- src/components/widgets/coins/coins.widget.tsx
+++ src/components/widgets/coins/coins.widget.tsx
@@ -17,24 +17,35 @@ const CoinsWidget = () => {
                 }} />
             </div>
             <div className="col-12">
-                <div className="list-group lh-lg small">{
-                    coins.length ? coins.map(([key, value]: [string, any]) => {
-                        return (
-                            <div className="list-group-item d-flex align-items-center justify-content-between py-3" key={key}>
-                                <div>
-                                    <span className="fw-regular">{key}</span>
-                                </div>
-                                <div className="text-end">
-                                    <span className="fw-regular">{Number(value?.rate).toFixed(2)}</span>
-                                </div>
-                            </div>
-                        )
-                    }) : (
-                        <div className="list-group-item text-center py-3">
-                            No data found
-                        </div>
-                    )
-                }</div>
+                {
+                    coins.length ? <div className="table-responsive">
+                        <table className="table lh-lg">
+                            <thead className="small">
+                                <tr>
+                                    <th>Name</th>
+                                    <th className="d-none d-md-block ">Key</th>
+                                    <th className="text-end text-md-start">Rate</th>
+                                </tr>
+                            </thead>
+                            <tbody className="small">
+                                {
+                                    coins.map(([key, value]: [string, any]) => {
+                                        return (
+                                            <tr key={key}>
+                                                <td className="py-3"><span>{key}</span>
+                                                    <p className="d-block d-md-none text-muted small mb-0">Key: {value?.key}</p></td>
+                                                <td className="d-none d-md-block py-3">{value?.key}</td>
+                                                <td className="text-end text-md-start py-3">{Number(value?.rate).toFixed(2)}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div> : <div className="text-center">
+                        <span>No data found</span>
+                    </div>
+                }
             </div>
         </div>
     )
