e1de36495323a7542ff8ee71caf9e34039fec222
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-10-07 20:07:05 +0100

    chore:

--- src/layouts/guest/guest.layout.tsx
+++ src/layouts/guest/guest.layout.tsx
@@ -13,7 +13,7 @@ export default function GuestLayout({
   return (
     <>
       <Head>
-        <title>{title ? `${title} - ` : ''}Bloom</title>
+      <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
