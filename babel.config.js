aad173edf7d685ef38e511a3654e07699ba59ab9
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-10-09 16:14:14 +0100

    chore:

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('babel').Config} */
-
-const babelConfig = {
-    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-};
-
-module.exports = babelConfig
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    transform: {
+        "\\.[jt]sx?$": "babel-jest",
+        "\\.css$": "some-css-transformer",
+    }
+}
 
 module.exports = nextConfig
