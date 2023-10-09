c0707c17195aa6a6bbfc1b77a5979249c539eda5
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-10-09 17:12:29 +0100

    chore:

--- src/lib/firebase/firebase.lib.tsx
+++ src/lib/firebase/firebase.lib.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCyC7i4Pxi8qkNZY4lqeDDOU3G_CwB5Jr0",
-    authDomain: "withbloom-app.firebaseapp.com",
-    projectId: "withbloom-app",
-    storageBucket: "withbloom-app.appspot.com",
-    messagingSenderId: "512746527582",
-    appId: "1:512746527582:web:6952ad6dada02f1f498dcf",
-    measurementId: "G-2P4TX40KN4"
+    apiKey: _config.FIREBASE_API_KEY,
+    authDomain: _config.FIREBASE_AUTH_DOMAIN,
+    projectId: _config.FIREBASE_PROJECT_ID,
+    storageBucket: _config.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: _config.FIREBASE_MESSAGING_SENDER_ID,
+    appId: _config.FIREBASE_APP_ID,
+    measurementId: _config.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
