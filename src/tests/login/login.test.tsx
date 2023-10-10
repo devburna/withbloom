7ec843658f46c67a2978354637fd987dc6f112c5
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-10-10 12:28:26 +0100

    chore:

--- src/tests/login/login.test.tsx
+++ src/tests/login/login.test.tsx
@@ -2,33 +2,41 @@ import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import LoginForm from '../../components/forms/login/login.form';
 import { LoadingProvider } from '../../context/loading/loading.context';
+import { useAuthContext } from '../../context/auth/auth.context';
 
+// Mock the useAuthContext hook to provide a mock login function
 jest.mock('../../context/auth/auth.context', () => ({
     useAuthContext: () => ({
         login: jest.fn(),
     }),
 }));
 
-test('User can login successfully', async () => {
+describe('User can login successfully', () => {
+    it('submits login form with user data', async () => {
+        // Render the LoginForm component wrapped in LoadingProvider
+        const { getByTestId } = render(
+            <LoadingProvider>
+                <LoginForm />
+            </LoadingProvider>
+        );
 
-    const { getByTestId } = render(
-        <LoadingProvider>
-            <LoginForm />
-        </LoadingProvider>
-    );
+        const email = 'devburna@gmail.com';
+        const password = 'devburna@gmail.com';
 
-    const email = 'devburna@gmail.com';
-    const password = 'devburna@gmail.com';
+        const emailInput = getByTestId('email');
+        const passwordInput = getByTestId('password');
+        const submitButton = getByTestId('submit');
 
-    const emailInput = getByTestId('email');
-    const passwordInput = getByTestId('password');
-    const submitButton = getByTestId('submit');
+        // Simulate user input
+        fireEvent.change(emailInput, { target: { value: email } });
+        fireEvent.change(passwordInput, { target: { value: password } });
 
-    fireEvent.change(emailInput, { target: { value: email } });
-    fireEvent.change(passwordInput, { target: { value: password } });
-    fireEvent.click(submitButton);
+        // Simulate form submission
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(jest.fn()).toHaveBeenCalledWith({ email, password });
+        // Wait for the login function to be called with the provided email and password
+        await waitFor(() => {
+            expect(useAuthContext().login).toHaveBeenCalledWith({ email, password });
+        });
     });
 });
--- src/tests/signup/signup.test.tsx
+++ src/tests/signup/signup.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import SignupForm from '../../components/forms/signup/signup.form';
 import { LoadingProvider } from '../../context/loading/loading.context';
+import { useAuthContext } from '../../context/auth/auth.context';
 
 jest.mock('../../context/auth/auth.context', () => ({
     useAuthContext: () => ({
@@ -9,29 +10,31 @@ jest.mock('../../context/auth/auth.context', () => ({
     }),
 }));
 
-test('User can signup successfully', async () => {
+describe('User can signup successfully', () => {
+    it('submits signup form with user data', async () => {
+        const { getByTestId } = render(
+            <LoadingProvider>
+                <SignupForm />
+            </LoadingProvider>
+        );
 
-    const { getByTestId } = render(
-        <LoadingProvider>
-            <SignupForm />
-        </LoadingProvider>
-    );
+        const username = 'Devburna';
+        const email = 'devburna@gmail.com';
+        const password = 'devburna@gmail.com';
 
-    const username = 'Devburna';
-    const email = 'devburna@gmail.com';
-    const password = 'devburna@gmail.com';
+        const usernameInput = getByTestId('username');
+        const emailInput = getByTestId('email');
+        const passwordInput = getByTestId('password');
+        const submitButton = getByTestId('submit');
 
-    const usernameInput = getByTestId('username');
-    const emailInput = getByTestId('email');
-    const passwordInput = getByTestId('password');
-    const submitButton = getByTestId('submit');
 
-    fireEvent.change(usernameInput, { target: { value: username } });
-    fireEvent.change(emailInput, { target: { value: email } });
-    fireEvent.change(passwordInput, { target: { value: password } });
-    fireEvent.click(submitButton);
+        fireEvent.change(usernameInput, { target: { value: username } });
+        fireEvent.change(emailInput, { target: { value: email } });
+        fireEvent.change(passwordInput, { target: { value: password } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(jest.fn()).toHaveBeenCalledWith({ username, email, password });
+        await waitFor(() => {
+            expect(useAuthContext().signup).toHaveBeenCalledWith({ username, email, password });
+        });
     });
 });
